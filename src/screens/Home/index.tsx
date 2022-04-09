fc1bad00b3a2ef455bbe596e1dca7cc1aaafefa1
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-04-09 09:57:38 -0300

    Estrutura inicial do projeto

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { HomeContainer, HomeTitle } from './styles';
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <HomeTitle>Home</HomeTitle>
+    </HomeContainer>
+  );
+}
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native'
+
+export const HomeContainer = styled.View`
+    justify-content: center;
+    align-items: center;
+    flex: 1;
+`
+
+export const HomeTitle = styled.Text`
+    color: #9f9f00;
+    font-size: 36px;
+    font-weight: bold;
+`
