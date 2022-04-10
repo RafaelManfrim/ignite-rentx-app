04bb9c8aa50f0aa8cbde0e9694774ac4039a19c6
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-04-09 23:54:35 -0300

    Criando CarList

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,21 +4,24 @@ import { RFValue } from 'react-native-responsive-fontsize';
 
 import Logo from '../../assets/logo.svg';
 
-import { HomeContainer, HomeHeader, HeaderContent, TotalCars } from './styles';
+import { HomeContainer, HomeHeader, HeaderContent, TotalCars, CarList } from './styles';
 import { Car } from '../../components/Car';
 
 export function Home() {
+  const car = { name: 'A3', brand: 'AUDI', rent: { price: 40, period: 'AO DIA' }, thumbnail: 'https://w7.pngwing.com/pngs/475/362/png-transparent-audi-sportback-concept-car-audi-a3-sportback-2015-audi-s3-audi-compact-car-sedan-car.png' }
+
   return (
     <HomeContainer>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
       <HomeHeader>
         <HeaderContent>
           <Logo height={RFValue(12)} width={RFValue(108)} />
-          <TotalCars>Total de 12 carros</TotalCars>
+          <TotalCars>Total de 8 carros</TotalCars>
         </HeaderContent>
       </HomeHeader>
-      <Car brand='AUDI' name='A3' rent={{ price: 40, period: 'AO DIA' }} thumbnail="https://w7.pngwing.com/pngs/475/362/png-transparent-audi-sportback-concept-car-audi-a3-sportback-2015-audi-s3-audi-compact-car-sedan-car.png" />
-      <Car brand='Ford' name='Mustang' rent={{ price: 95, period: 'AO DIA' }} thumbnail="https://w7.pngwing.com/pngs/475/362/png-transparent-audi-sportback-concept-car-audi-a3-sportback-2015-audi-s3-audi-compact-car-sedan-car.png" />
+      <CarList data={[1, 2, 3, 4, 5, 6, 7, 8]} keyExtractor={item => String(item)} renderItem={({ item }) => (
+        <Car brand={car.brand} name={car.name} rent={car.rent} thumbnail={car.thumbnail} />
+      )} />
     </HomeContainer>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,3 +1,4 @@
+import { FlatList } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native'
 
@@ -26,3 +27,10 @@ export const TotalCars = styled.Text`
     font-size: ${RFValue(14)}px;
     font-family: ${({ theme }) => theme.fonts.primary_400};
 `;
+
+export const CarList = styled(FlatList).attrs({
+    contentContainerStyle: {
+        padding: 24
+    },
+    showsVerticalScrollIndicator: false
+})``;
