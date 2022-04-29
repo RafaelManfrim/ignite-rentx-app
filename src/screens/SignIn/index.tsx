522fe8d12eaba7043540a5525af54d9d9151a992
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-04-29 18:49:10 -0300

    Acessibilidade do Form de login

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
 import { useTheme } from 'styled-components';
+import {
+  StatusBar,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard
+} from 'react-native';
 
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
@@ -19,46 +24,50 @@ export function SignIn() {
   const { colors } = useTheme()
 
   return (
-    <SignInMainContainer>
-      <StatusBar translucent barStyle="dark-content" backgroundColor="transparent" />
-      <SignInHeader>
-        <SignInTitle>{'Estamos \nquase lá'}</SignInTitle>
-        <SignInSubTitle>
-          {'Faça seu login para começar \numa experiência incrível.'}
-        </SignInSubTitle>
-      </SignInHeader>
-      <InputsArea>
-        <Input
-          iconName="mail"
-          placeholder="E-mail"
-          keyboardType='email-address'
-          autoCorrect={false}
-          autoCapitalize='none'
-        />
-        <PasswordInput
-          iconName="lock"
-          placeholder="Senha"
-          autoCorrect={false}
-          autoCapitalize='none'
-        />
-      </InputsArea>
-      <ButtonsArea>
-        <Button
-          title="Login"
-          loading={false}
-          enabled={true}
-          onPress={() => { }}
-          style={{ marginBottom: 8 }}
-        />
-        <Button
-          title="Criar uma conta gratuíta"
-          loading={false}
-          enabled={true}
-          onPress={() => { }}
-          color={colors.background_secondary}
-          light
-        />
-      </ButtonsArea>
-    </SignInMainContainer>
+    <KeyboardAvoidingView behavior='position' enabled>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <SignInMainContainer>
+          <StatusBar translucent barStyle="dark-content" backgroundColor="transparent" />
+          <SignInHeader>
+            <SignInTitle>{'Estamos \nquase lá'}</SignInTitle>
+            <SignInSubTitle>
+              {'Faça seu login para começar \numa experiência incrível.'}
+            </SignInSubTitle>
+          </SignInHeader>
+          <InputsArea>
+            <Input
+              iconName="mail"
+              placeholder="E-mail"
+              keyboardType='email-address'
+              autoCorrect={false}
+              autoCapitalize='none'
+            />
+            <PasswordInput
+              iconName="lock"
+              placeholder="Senha"
+              autoCorrect={false}
+              autoCapitalize='none'
+            />
+          </InputsArea>
+          <ButtonsArea>
+            <Button
+              title="Login"
+              loading={false}
+              enabled={true}
+              onPress={() => { }}
+              style={{ marginBottom: 8 }}
+            />
+            <Button
+              title="Criar uma conta gratuíta"
+              loading={false}
+              enabled={true}
+              onPress={() => { }}
+              color={colors.background_secondary}
+              light
+            />
+          </ButtonsArea>
+        </SignInMainContainer>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
