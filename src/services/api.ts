a483d205d8c2a4460ae9668369c97f36fd99ca88
RafaelManfrim <rafael.manfrim@etec.sp.gov.br>
2022-06-19 15:51:53 -0300

    ip da api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios"
 
-const baseURL = 'http://10.0.0.106:3333'
+const baseURL = 'http://192.168.0.116:3333'
+// const baseURL = 'http://10.0.0.106:3333'
 // const baseURL = 'http://192.168.1.21:3333'
 
 export const api = axios.create({ baseURL })
